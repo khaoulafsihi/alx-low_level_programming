741c4141962c681e201090583f1a69b8e77cfe94
khaoulafsihi <khaoulafsihi@gmail.com>
2023-03-16 11:46:08 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that prints a line with puts function 
  * Return: 0 (Sucess)
  */
-int main (void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main (void)
+{puts("\"Programming is like building a multilingual puzzle);
+return(0);
 }

d8a2c50dba500232a2ea24574811b5f0e2fa58e6
khaoulafsihi <khaoulafsihi@gmail.com>
2023-03-16 11:48:17 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@
  * Return: 0 (Sucess)
  */
 int main (void)
-{puts("\"Programming is like building a multilingual puzzle);
+{
+puts("\"Programming is like building a multilingual puzzle);
 return(0);
 }

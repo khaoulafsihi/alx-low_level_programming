f2418adf3cc9b3f3a89f785cffb0fd29742beba8
khaoulafsihi <khaoulafsihi@gmail.com>
2023-03-21 09:40:51 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
 #include "main.h"
 	/**
-	 * main - print _putchar
+	 * main - Entry point
 	 * is written by khaoula
-	 * Return: 0
+	 * Return:always 0
 	 */
 	int main(void)
 	{

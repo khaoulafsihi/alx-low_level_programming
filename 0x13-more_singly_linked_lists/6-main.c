8c9e9b2532b5c892934877df11056051629bdcb9
khaoulafsihi <khaoulafsihi@gmail.com>
2023-05-01 11:14:09 -0700

    Task6

--- 0x13-more_singly_linked_lists/6-main.c
+++ 0x13-more_singly_linked_lists/6-main.c
@@ -1,20 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
-* pop_listint - deletes the head node of a linked list
-* @head: pointer to the first element in the linked list
-* Return: the data inside the elements that was deleted,
-* or 0 if the list is empty
-*/
-int pop_listint(listint_t **head)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-listint_t *temp;
-int num;
-if (!head || !*head)
-return (0);
-num = (*head)->n;
-temp = (*head)->next;
-free(*head);
-*head = temp;
-return (num);
+    listint_t *head;
+    int n;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    n = pop_listint(&head);
+    printf("- %d\n", n);
+    print_listint(head);
+    n = pop_listint(&head);
+    printf("- %d\n", n);
+    print_listint(head);
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
+    return (0);
 }
 

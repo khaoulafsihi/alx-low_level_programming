bacc21ce25c2e3cedfa151970926bbf4e42130ed
khaoulafsihi <khaoulafsihi@gmail.com>
2023-05-09 11:49:06 -0700

    Task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* read_textfile- Read text file print to STDOUT.
+* @filename: text file being read
+* @letters: number of letters to be read
+* Return: w- actual number of bytes read and printed
+*        0 when function fails or filename is NULL.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buf;
+ssize_t j;
+ssize_t w;
+ssize_t t;
+fd = open(filename, O_RDONLY);
+if (j == -1)
+return (0);
+buf = malloc(sizeof(char) * letters);
+t = read(j, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
+free(buf);
+close(j);
+return (w);
+}
+
